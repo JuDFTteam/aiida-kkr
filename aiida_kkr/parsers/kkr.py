9aa378e3c1775160af0f6bd50736e68fa6b2dff9
broeder <j.broeder@fz-juelich.de>
2017-11-13 09:47:29 +0100

    small bugfix in kkrparser, parsing some output for tests

--- aiida_kkr/parsers/kkr.py
+++ aiida_kkr/parsers/kkr.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+"""
+Parser for the KKR Code.
+The parser should never fail, but it should catch 
+all errors and warnings and show them to the user.
+"""
 
 from aiida.parsers.parser import Parser
 from aiida.orm.data.parameter import ParameterData
@@ -41,19 +46,64 @@ class KkrParser(Parser):
             self.logger.error("Output file not found")
             return success, node_list
 
-        try:
+        try: # TODO: open here or in subroutine?
             with open(out_folder.get_abs_path(
                     self._calc._OUTPUT_FILE_NAME)) as f:
-                #out_dict = json.load(f)
-                pass
+                outtxt = f.readlines()
+                result_dict = parse_kkr_outputfile(outtxt)
         except ValueError:
             self.logger.error("Error parsing the output json")
             return success, node_list
         
-        out_dict = {}
+        out_dict = result_dict
+        
+        #TODO other keys to add...
+        
         output_data = ParameterData(dict=out_dict)
         link_name = self.get_linkname_outparams()
         node_list = [(link_name, output_data)]
         success = True
 
         return success, node_list
+
+def parse_kkr_outputfile(outfile):
+    """
+    Parser method for the kkr outfile. It returns a dictionary with results
+    """
+    # TODO This still needs to be overworked.
+    # If we want to store the arrays we have to store them as array data
+    # If only certain results are imported they have to be distilled and stored in the output node
+    
+    from scipy import array
+    
+    #parse out-file
+    #get rms error and other relevant output-data
+    results = {}
+    #outfi = open(outfile, 'r') # TODO try block
+    outtxt = outfile#outfile.readlines()
+    try:
+        results['rms'] = array([float(outtxt[iline].split('=')[1].replace('D', 'E')) for iline in range(len(outtxt)) if 'average rms-error' in outtxt[iline]])
+    except:
+        results['rms'] = array([-1.0])
+    try:
+        results['charge_neutrality'] = array([float(outtxt[iline].split('=')[1]) for iline in range(len(outtxt)) if 'charge neutrality in unit cell' in outtxt[iline]])
+    except:
+        results['charge_neutrality'] = array([-1.0])
+    try:
+        results['total_magnetic_moment'] = array([float(outtxt[iline].split('=')[1]) for iline in range(len(outtxt)) if 'TOTAL mag. moment in unit cell' in outtxt[iline]])
+    except:
+        results['total_magnetic_moment'] = array([-1.0])
+    try:
+        results['EF'] = array([float(outtxt[iline].split('FERMI')[1].split()[0]) for iline in range(len(outtxt)) if 'E FERMI' in outtxt[iline]])
+    except:
+        results['EF'] = array([-1.0])
+    try:
+        results['DOS_EF'] = array([float(outtxt[iline].split('=')[1]) for iline in range(len(outtxt)) if 'DOS(E_F)' in outtxt[iline]])
+    except:
+        results['DOS_EF'] = array([-1.0])
+    try:
+        results['total_energy'] = array([float(outtxt[iline].split(':')[1]) for iline in range(len(outtxt)) if 'TOTAL ENERGY in ryd.' in outtxt[iline]])
+    except:
+        results['total_energy'] = array([-1.0])
+        
+    return results
