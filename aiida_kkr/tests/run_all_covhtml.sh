757d229807c9ff7b5206450b9af5b80549654165
ruess <p.ruessmann@fz-juelich.de>
2018-11-27 11:11:05 +0100

    Include full kkrimp test

--- aiida_kkr/tests/run_all_covhtml.sh
+++ aiida_kkr/tests/run_all_covhtml.sh
@@ -44,6 +44,7 @@
 #pytest -s --cov-report=html --cov=aiida_kkr -k Test_scf_workflow
 #pytest -s --cov-report=html --cov=aiida_kkr -k Test_vorostart_workflow
 
-#pytest --cov-report=html --cov=aiida_kkr --ignore=test_scf_wc_simple.py 
+pytest --cov-report=html --cov=aiida_kkr --ignore=test_scf_wc_simple.py 
 #pytest -s --cov-report=html --cov=aiida_kkr -k Test_gf_writeout_workflow
-pytest -s --cov-report=html --cov=aiida_kkr -k Test_kkrimp_scf_workflow
+#pytest -s --cov-report=html --cov=aiida_kkr -k Test_kkrimp_scf_workflow
+#pytest -s --cov-report=html --cov=aiida_kkr -k Test_kkrimp_full_workflow
