ee08ee61b1a0a917bd1260438bc25bd9866c64af
ruess <p.ruessmann@fz-juelich.de>
2019-03-14 16:45:25 +0100

    Move dos workflow test to standard tests

--- aiida_kkr/tests/run_all.sh
+++ aiida_kkr/tests/run_all.sh
@@ -4,4 +4,4 @@ mkdir -p '.aiida';
 #pytest -sv
 #pytest -v
 #pytest --cov-report=term-missing --cov=aiida_kkr --ignore=test_scf_wc_simple.py
-pytest --cov-report=term-missing --cov=aiida_kkr --ignore=test_scf_wc_simple.py --ignore=test_vorostart_wc.py --ignore=test_dos_wc.py --ignore=test_gf_writeout_wc.py --ignore=test_kkrimp_sub_wc.py --ignore=test_kkrimp_full_wc.py --ignore=jukkr
+pytest --cov-report=term-missing --cov=aiida_kkr --ignore=test_scf_wc_simple.py --ignore=test_vorostart_wc.py --ignore=test_gf_writeout_wc.py --ignore=test_kkrimp_sub_wc.py --ignore=test_kkrimp_full_wc.py --ignore=jukkr
