6a2da26c7c9fd516df8f617f2b9739fdd974ebf6
ruess <p.ruessmann@fz-juelich.de>
2019-03-15 09:31:11 +0100

    Add check to make sure rabbit mq is working and enforce pika==1.0.0b1

---
+++ aiida_kkr/tests/verify_rmq_check.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from masci_tools.io.common_functions import search_string
+with open('out_rmq_check') as f:
+    txt = f.readlines()
+    i = search_string('Sent', txt)
+    t_sent = txt.pop(i)
+    t_sent = t_sent.split('Hello World! ')[1]
+    j = search_string(t_sent, txt)
+    print 'rmq check passed?', t_sent in txt[j]
+    assert t_sent in txt[j]
+
