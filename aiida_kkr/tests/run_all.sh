82c5f9bc646367b88cdb386345d273b4e7d41afd
ruess <p.ruessmann@fz-juelich.de>
2019-04-29 15:02:29 +0200

    Activate tests that should run on travis

--- aiida_kkr/tests/run_all.sh
+++ aiida_kkr/tests/run_all.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 export AIIDA_PATH='.';
 mkdir -p '.aiida';
-#pytest -sv
+
+#pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=jukkr --mpl -p no:warnings
 
 # tests without running actual calculations
 pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=workflows --ignore=jukkr --mpl -p no:warnings
@@ -10,10 +11,10 @@ pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=workflows
 pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=jukkr -k Test_vorostart_workflow
 
 # these tests fail at the moment because gfortran is too old
-pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=jukkr -k Test_dos_workflow
-pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=jukkr -k Test_gf_writeout_workflow
-pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=jukkr -k Test_scf_workflow
-pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=jukkr -k Test_eos_workflow
+#pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=jukkr -k Test_dos_workflow
+#pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=jukkr -k Test_gf_writeout_workflow
+#pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=jukkr -k Test_scf_workflow
+#pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=jukkr -k Test_eos_workflow
 
 # these are deactivated becaus gfortran compilation of kkrimp does not work
 #pytest --cov-report=term-missing --cov-append --cov=aiida_kkr --ignore=jukkr -k Test_kkrimp_scf_workflow
--- aiida_kkr/tests/run_all_covhtml.sh
+++ aiida_kkr/tests/run_all_covhtml.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env sh
 
-#pytest -v --cov-report=html --cov=aiida_kkr --ignore=test_entrypoints.py --ignore=test_scf_wc_simple.py --ignore=test_common_workfunctions.py
-#pytest -v --cov-report=html --cov=aiida_kkr --ignore=test_scf_wc_simple.py
-#pytest -s --cov-report=html --cov=aiida_kkr --ignore=test_scf_wc_simple.py 
-#pytest -s --cov-report=html --cov=aiida_kkr -k Test_vorostart_workflow
-#pytest -s --cov-report=html --cov=aiida_kkr -k Test_kkrimp_full_workflow
-#pytest -s --cov-report=html --cov=aiida_kkr -k Test_scf_workflow
-
-#pytest -s --cov-report=html --cov=aiida_kkr --ignore=jukkr -k Test_dos_workflow
-#pytest -s --cov-report=html --cov=aiida_kkr --ignore=jukkr -k Test_gf_writeout_workflow -k Test_dos_workflow
-#pytest --cov-report=html --cov=aiida_kkr --ignore=jukkr --ignore=test_scf_wc_simple.py --ignore=test_vorostart_wc.py --ignore=test_kkrimp_sub_wc.py --ignore=test_kkrimp_full_wc.py
 pytest -v -s --cov-report=html --cov=aiida_kkr --ignore=jukkr --mpl -p no:warnings
 #pytest -v -s --cov-report=html --cov=aiida_kkr --ignore=jukkr --ignore=workflows --mpl -p no:warnings
