f2160acaeaba5b76821ea22090e8d6ed00082337
RubelMozumder <rubel.mozumder@outlook.com>
2021-05-31 13:54:21 +0000

    adding the combine

--- aiida_kkr/workflows/_combine_imps.py
+++ aiida_kkr/workflows/_combine_imps.py
@@ -267,7 +267,7 @@ If given then the writeout step of the host GF is omitted.""")
 
             if not (imp_calc_or_wf.process_class == kkr_imp_wc or 
                     imp_calc_or_wf.process_class == kkr_imp_sub_wc or
-                     imp_calc_or_wf.process_class == self
+                     imp_calc_or_wf.process_class == self.__class__
                     ):
                 self.report("impurity_workflow class is wrong: {}".format(imp_calc_or_wf))
                 return False
@@ -277,15 +277,11 @@ If given then the writeout step of the host GF is omitted.""")
                 if not imp_calc_or_wf.outputs.workflow_info.get_dict().get('converged'):
                     self.report("impurity_workflow not converged")
                     return False
-            elif imp_calc_or_wf.process_class==kkr_imp_sub_wc:
+            elif (imp_calc_or_wf.process_class == kkr_imp_sub_wc or 
+                imp_calc_or_wf.process_class == self.__class__):
                 if not imp_calc_or_wf.outputs.workflow_info.get_dict().get('convergence_reached'):
                     self.report("impurity_workflow not converged")
                     return False
-            elif imp_calc_or_wf.process_class==self:
-                if not imp_calc_or_wf.outputs.workflow_info.get_dict().get('convergence_reached'):
-                    self.report("impurity_workflow not converged")
-                    return False
-
 
         # all checks passed
         return True
