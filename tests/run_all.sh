d90ac3e35b85a4467d39f32a91aecff06f16bfdb
Philipp Rüßmann <10085427+PhilippRue@users.noreply.github.com>
2024-11-25 15:15:35 +0000

    activate STM test in CI

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -125,6 +125,7 @@ elif [[ ! -z "$GITHUB_SUITE" ]]; then
 	  ./workflows/test_decimate.py \
 	  ./workflows/test_kkrimp_dos_wc.py \
 	  ./workflows/test_kkrimp_full_wc.py \
+	  ./workflows/test_stm.py \
 	  $addopt
   # full list, excludes:
   #  - currently tests for kkrimp_sub, jij and combine_imps are not working
